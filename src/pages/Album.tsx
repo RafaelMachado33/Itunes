45e3f23c2b70592e38822302aaee5b7a7ab08396
Rafael Machado <machadorafael27@gmail.com>
2023-10-04 23:31:04 -0300

    exercise 2 trying

--- src/pages/Album.tsx
+++ src/pages/Album.tsx
@@ -1,7 +1,43 @@
-function Album() {
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AlbumType, SongType } from '../types';
+
+type Data =
+ [ {
+   artistId: 12,
+   artistName: 'Artist Name',
+   collectionId: 123,
+   collectionName: 'Collection Name',
+   collectionPrice: 12.25,
+   artworkUrl100: 'https://url-to-image',
+   releaseDate: '2012-03-02T08:00:00Z',
+   trackCount: 8,
+ } ];
+
+function Album({ data }:Data) {
+  // const [loading, setLoading] = useState(false);
+  /* Em vez do music no map, poderia colocar a desestruturação
+ com as propriedades necessarias. Como no exempo abaixo poderia substituir o
+  music por {collectionId,collectionName} */
+
+  console.log(data[0]);
   return (
-    <h1> Album</h1>
+    <ul>
+      {data.map((music) => (
+        <li key={ music.collectionId }>
+          <h2>
+            {music.collectionName}
+            {' '}
+          </h2>
+          <Link to="/album/:id">Link</Link>
+
+        </li>
+      ))}
+
+    </ul>
   );
 }
 
 export default Album;
+
+// <Album data={ artista } />
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from '../componetes/Carregando';
+import { AlbumType, SongType } from '../types';
+import Album from './Album';
 
 function Search() {
   const [name, setname] = useState('');
+  const [name2, setname2] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [music, setmusic] = useState();
+  // const [data, setdata] = useState();
 
   let validador = false;
 
@@ -12,26 +19,51 @@ function Search() {
 
   async function handleSubmit(event:React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     event.preventDefault();
+    setLoading(true);
     const artista = await searchAlbumsAPI(name);
-    console.log(artista);
+    setmusic(artista);
+    setname2(name);
+    setname('');
+    setLoading(false);
+    // setdata(artista);
+    // console.log(artista[0].artistName);
   }
-
+  // console.log(music);
+  if (loading) {
+    return <Carregando />;
+  }
   return (
-    <form>
-      <input
-        type="text"
-        data-testid="search-artist-input"
-        value={ name }
-        onChange={ (event) => setname(event.target.value) }
-      />
-      <button
-        data-testid="search-artist-button"
-        disabled={ validador === false }
-        onClick={ (event) => handleSubmit(event) }
-      >
-        Pesquisar
-      </button>
-    </form>
+    <>
+      <form>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          value={ name }
+          onChange={ (event) => setname(event.target.value) }
+        />
+        <button
+          data-testid="search-artist-button"
+          disabled={ validador === false }
+          onClick={ (event) => handleSubmit(event) }
+        >
+          Pesquisar
+        </button>
+      </form>
+      {name2
+        ? (
+          <>
+            <p>
+              Resultado de álbuns de:
+              {' '}
+              {name2}
+            </p>
+            {music.length >= 1
+              ? <Album data={ music } /> : <p>Nenhum álbum foi encontrado</p>}
+          </>
+        ) : ''}
+
+    </>
+
   );
 }
 
